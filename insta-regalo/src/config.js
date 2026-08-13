9ea5977cdf2fb528215d733af1f0e7943c09217d
ItsMeZeVas <veralegrojuansebastian@gmail.com>
2026-08-12 20:38:16 -0500

    Update config.js

--- insta-regalo/src/config.js
+++ insta-regalo/src/config.js
@@ -13,4 +13,10 @@ export const MASTHEAD_HEADLINE = 'RECUERDOS'
 
 // ---- Mensaje especial que aparece de vez en cuando en una burbuja ----
 export const EXTRA_MESSAGE_TITLE = 'EDICIÓN DE ÚLTIMA HORA'
-export const EXTRA_MESSAGE_BODY = 'Escribe aquí el mensaje que quieras que tu amigo lea cuando toque la burbuja.'
+export const EXTRA_MESSAGE_BODY = `Buenas bro, sé que casi nunca te digo estas cosas, pero te las quiero dejar plasmadas en esta nueva noticia del Daily Bugle para que nunca la olvides y la puedas volver a leer cuando quieras. Antes que nada, quiero que sepas que este mensaje no es solo de cumpleaños, es de cualquier día, quiero que esté por ahí para cuando lo necesites.
+
+Durante todo el tiempo que te conozco me has demostrado que eres una persona increíble, que siempre busca sacar lo mejor de los demás y que se entristece cuando a otros les va mal, y eso ya dice mucho del corazón que tienes. Sé que a veces la vida pega duro, pero también sé que sea cual sea el desafío que se te ponga por delante, lo vas a superar, y si no lo logras a la primera, lo vas a intentar hasta lograrlo.
+
+Eres mi mejor amigo, y si en algún momento necesitas ayuda, no dudes en buscar mi hombro para apoyarte. Nunca olvides que me siento muy afortunado de tenerte en mi vida, y espero seguir compartiendo contigo así terminemos la universidad, y si logramos seguir juntos después de eso, pues god, no?
+
+Feliz cumpleaños chaval, te mereces todo lo bueno que hay en este mundo.`
